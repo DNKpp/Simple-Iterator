a6361a6243cd2994efc6d8b087abf375ec6114a9
DNKpp <DNKpp2011@gmail.com>
2021-06-07 19:50:23 +0200

    rename IteratorTests.cpp to IteratorConceptTests.cpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- test/IteratorConceptTests.cpp
+++ test/IteratorConceptTests.cpp
@@ -5,12 +5,12 @@
 
 #include <catch2/catch.hpp>
 
-#include "Simple-Iterator/iterator.hpp"
-#include "Simple-Iterator/input_iterator.hpp"
-#include "Simple-Iterator/forward_iterator.hpp"
 #include "Simple-Iterator/bidirectional_iterator.hpp"
-#include "Simple-Iterator/random_access_iterator.hpp"
 #include "Simple-Iterator/contiguous_iterator.hpp"
+#include "Simple-Iterator/forward_iterator.hpp"
+#include "Simple-Iterator/input_iterator.hpp"
+#include "Simple-Iterator/iterator.hpp"
+#include "Simple-Iterator/random_access_iterator.hpp"
 
 using namespace sl::itr;
 
@@ -20,7 +20,7 @@ namespace
 	{
 		int m = 0;
 	};
-	
+
 	// there is no general iterator concept, thus I have to gather all requirements myself.
 	// https://en.cppreference.com/w/cpp/named_req/Iterator
 	template <class TItr>
@@ -33,7 +33,8 @@ namespace
 	};
 
 	class TestIterator :
-		public detail::base_iterator<TestValueType, TestIterator, TestValueType*, int, std::plus<>{}, dereference{}, std::input_iterator_tag>
+		public detail::base_iterator<TestValueType, TestIterator, TestValueType*, int, std::plus<>{}, dereference{},
+									std::input_iterator_tag>
 	{
 	public:
 		TestIterator() noexcept = default;
@@ -75,22 +76,25 @@ namespace
 	};
 }
 
-TEMPLATE_TEST_CASE(
-					"iterators should match all concepts of LegacyIterator",
-					"[iterator]",
-					TestIterator,
-					TestInputIterator,
-					TestForwardIterator,
-					TestBidirectionalIterator,
-					TestRandomAccessIterator,
-					TestContiguousIterator,
+#pragma warning(disable: 26444)
+TEMPLATE_TEST_CASE
+(
+	"iterators should match all concepts of LegacyIterator",
+	"[iterator]",
+	TestIterator,
+	TestInputIterator,
+	TestForwardIterator,
+	TestBidirectionalIterator,
+	TestRandomAccessIterator,
+	TestContiguousIterator,
 
-					input_iterator<int>,
-					forward_iterator<int>,
-					bidirectional_iterator<int>,
-					random_access_iterator<int>,
-					contiguous_iterator<int>
-				)
+	input_iterator<int>,
+	forward_iterator<int>,
+	bidirectional_iterator<int>,
+	random_access_iterator<int>,
+	contiguous_iterator<int>
+)
+#pragma warning(default: 26444)
 {
 	using Iterator_t = TestType;
 
@@ -100,21 +104,24 @@ TEMPLATE_TEST_CASE(
 	REQUIRE(legacy_iterator<Iterator_t>);
 }
 
-TEMPLATE_TEST_CASE(
-					"iterators should match all concepts of std::input_iterator",
-					"[iterator][input_iterator]",
-					TestInputIterator,
-					TestForwardIterator,
-					TestBidirectionalIterator,
-					TestRandomAccessIterator,
-					TestContiguousIterator,
+#pragma warning(disable: 26444)
+TEMPLATE_TEST_CASE
+(
+	"iterators should match all concepts of std::input_iterator",
+	"[iterator][input_iterator]",
+	TestInputIterator,
+	TestForwardIterator,
+	TestBidirectionalIterator,
+	TestRandomAccessIterator,
+	TestContiguousIterator,
 
-					input_iterator<int>,
-					forward_iterator<int>,
-					bidirectional_iterator<int>,
-					random_access_iterator<int>,
-					contiguous_iterator<int>
-				)
+	input_iterator<int>,
+	forward_iterator<int>,
+	bidirectional_iterator<int>,
+	random_access_iterator<int>,
+	contiguous_iterator<int>
+)
+#pragma warning(default: 26444)
 {
 	using Iterator_t = TestType;
 
@@ -130,19 +137,22 @@ TEMPLATE_TEST_CASE(
 	REQUIRE(std::input_iterator<Iterator_t>);
 }
 
-TEMPLATE_TEST_CASE(
-					"iterators should match all concepts of std::forward_iterator",
-					"[iterator][input_iterator][forward_iterator]",
-					TestForwardIterator,
-					TestBidirectionalIterator,
-					TestRandomAccessIterator,
-					TestContiguousIterator,
+#pragma warning(disable: 26444)
+TEMPLATE_TEST_CASE
+(
+	"iterators should match all concepts of std::forward_iterator",
+	"[iterator][input_iterator][forward_iterator]",
+	TestForwardIterator,
+	TestBidirectionalIterator,
+	TestRandomAccessIterator,
+	TestContiguousIterator,
 
-					forward_iterator<int>,
-					bidirectional_iterator<int>,
-					random_access_iterator<int>,
-					contiguous_iterator<int>
-				)
+	forward_iterator<int>,
+	bidirectional_iterator<int>,
+	random_access_iterator<int>,
+	contiguous_iterator<int>
+)
+#pragma warning(default: 26444)
 {
 	using Iterator_t = TestType;
 
@@ -153,17 +163,20 @@ TEMPLATE_TEST_CASE(
 	REQUIRE(std::forward_iterator<Iterator_t>);
 }
 
-TEMPLATE_TEST_CASE(
-					"iterators should match all concepts of std::bidirectional_iterator",
-					"[iterator][input_iterator][forward_iterator][bidirectional_iterator]",
-					TestBidirectionalIterator,
-					TestRandomAccessIterator,
-					TestContiguousIterator,
+#pragma warning(disable: 26444)
+TEMPLATE_TEST_CASE
+(
+	"iterators should match all concepts of std::bidirectional_iterator",
+	"[iterator][input_iterator][forward_iterator][bidirectional_iterator]",
+	TestBidirectionalIterator,
+	TestRandomAccessIterator,
+	TestContiguousIterator,
 
-					bidirectional_iterator<int>,
-					random_access_iterator<int>,
-					contiguous_iterator<int>
-				)
+	bidirectional_iterator<int>,
+	random_access_iterator<int>,
+	contiguous_iterator<int>
+)
+#pragma warning(default: 26444)
 {
 	using Iterator_t = TestType;
 
@@ -171,15 +184,18 @@ TEMPLATE_TEST_CASE(
 	REQUIRE(std::bidirectional_iterator<Iterator_t>);
 }
 
-TEMPLATE_TEST_CASE(
-					"iterators should match all concepts of std::random_access_iterator",
-					"[iterator][input_iterator][forward_iterator][bidirectional_iterator][random_access_iterator]",
-					TestRandomAccessIterator,
-					TestContiguousIterator,
+#pragma warning(disable: 26444)
+TEMPLATE_TEST_CASE
+(
+	"iterators should match all concepts of std::random_access_iterator",
+	"[iterator][input_iterator][forward_iterator][bidirectional_iterator][random_access_iterator]",
+	TestRandomAccessIterator,
+	TestContiguousIterator,
 
-					random_access_iterator<int>,
-					contiguous_iterator<int>
-				)
+	random_access_iterator<int>,
+	contiguous_iterator<int>
+)
+#pragma warning(default: 26444)
 {
 	using Iterator_t = TestType;
 
@@ -190,13 +206,16 @@ TEMPLATE_TEST_CASE(
 	REQUIRE(std::random_access_iterator<Iterator_t>);
 }
 
-TEMPLATE_TEST_CASE(
-					"iterators should match all concepts of std::contiguous_iterator",
-					"[iterator][input_iterator][forward_iterator][bidirectional_iterator][random_access_iterator][contiguous_iterator]",
-					TestContiguousIterator,
+#pragma warning(disable: 26444)
+TEMPLATE_TEST_CASE
+(
+	"iterators should match all concepts of std::contiguous_iterator",
+	"[iterator][input_iterator][forward_iterator][bidirectional_iterator][random_access_iterator][contiguous_iterator]",
+	TestContiguousIterator,
 
-					contiguous_iterator<int>
-				)
+	contiguous_iterator<int>
+)
+#pragma warning(default: 26444)
 {
 	using Iterator_t = TestType;
 
